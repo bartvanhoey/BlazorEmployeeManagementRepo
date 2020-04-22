795245d76656c64d2d4acceca8bfd06012f84ce3
bartvanhoey <bartvanhoey@hotmail.com>
2020-04-22 20:24:18 +0200

    end-part-13

---
+++ EmployeeManagement.API/Models/DepartmentRepository.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.Models;
+using System.Collections.Generic;
+using System.Linq;
+using EmployeeManagement.API.Models;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public DepartmentRepository(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+        }
+
+        public Department GetDepartment(int departmentId)
+        {
+            return _dbContext.Departments
+                .FirstOrDefault(d => d.DepartmentId == departmentId);
+        }
+
+        public IEnumerable<Department> GetDepartments()
+        {
+            return _dbContext.Departments;
+        }
+    }
+}
