a770948ba0e8a74e841c2f171bd47602ee22cba6
bartvanhoey <bartvanhoey@hotmail.com>
2020-04-28 15:23:51 +0200

    end-part-23

--- EmployeeManagement.API/Models/EmployeeRepository.cs
+++ EmployeeManagement.API/Models/EmployeeRepository.cs
@@ -24,6 +24,7 @@ namespace EmployeeManagement.Api.Models
         public async Task<Employee> GetEmployee(int employeeId)
         {
             return await _dbContext.Employees
+                .Include(e => e.Department)
                 .FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
         }
 
