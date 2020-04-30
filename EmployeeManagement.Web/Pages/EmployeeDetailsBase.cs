15c449a9edd9b43e7e2bf48d592352c45c69d298
bartvanhoey <bartvanhoey@hotmail.com>
2020-04-30 07:21:34 +0200

    end-part-24

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -2,12 +2,18 @@ using System.Threading.Tasks;
 using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace EmployeeManagement.Web.Pages
 {
     public class EmployeeDetailsBase : ComponentBase
     {
         public Employee Employee { get; set; } = new Employee();
+
+        public string Coordinates { get; set; }
+
+        public string ButtonText { get; set; } = "Hide Footer";
+        public string CssClass { get; set; } = null;
 
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
@@ -21,6 +27,23 @@ namespace EmployeeManagement.Web.Pages
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
         }
 
+        // public void Mouse_Move(MouseEventArgs e) { 
+        //     Coordinates = $"X = {e.ClientX} - Y = {e.ClientY}";
+        // }
+
+        public void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "hideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
 
 
     }
