846c0b6fdb19f9e8987c90c72fd46bc8ef86f0d2
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-22 13:57:00 +0200

    end-part-44

--- EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -1,11 +1,16 @@
 using System.Threading.Tasks;
 using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace EmployeeManagement.Web.Pages
 {
     public class DisplayEmployeeBase : ComponentBase
     {
+       
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
         [Parameter]
         public Employee Employee { get; set; }
 
@@ -15,9 +20,17 @@ namespace EmployeeManagement.Web.Pages
         [Parameter]
         public EventCallback<bool> OnEmployeeSelection { get; set; }
 
+         [Parameter]
+        public EventCallback<int> OnEmployeeDeleted { get; set; }
+
         public async Task CheckBoxChanged(ChangeEventArgs e)
         {
             await OnEmployeeSelection.InvokeAsync((bool)e.Value);
+        }
+
+        public async Task Delete_Click() {
+            await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+            await OnEmployeeDeleted.InvokeAsync(Employee.EmployeeId);
         }
     }
 }
